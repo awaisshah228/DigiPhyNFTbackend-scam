e864223c50878feef9022cfc526d13c37439590c
aman <aman.espsofttech@gmail.com>
2022-12-26 18:43:43 +0530

    updated

--- routes/routes.js
+++ routes/routes.js
@@ -69,7 +69,7 @@ var sliderUpload = upload.fields([{ name: 'slider1', maxCount: 1 }, { name: 'sli
 
 var collectionImages = upload.fields([{ name: 'profile_pic', maxCount: 1 }, { name: 'banner', maxCount: 1 }]);
 
-var addnftImage = upload.fields([{ name: 'image', maxCount: 1 }
+var addnftImage = upload.fields([{ name: 'local_image', maxCount: 1 }
 // ,{name:'image_file_scanner',maxCount:1}
 ])
 
